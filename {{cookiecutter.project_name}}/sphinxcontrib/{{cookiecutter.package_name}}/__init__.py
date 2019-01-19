53b53871645e60f0bd9c5039a30c3bca7c426fc1
Takeshi KOMIYA <i.tkomiya@gmail.com>
2019-01-19 17:31:47 +0900

    i18n

--- {{cookiecutter.project_name}}/sphinxcontrib/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_name}}/sphinxcontrib/{{cookiecutter.package_name}}/__init__.py
@@ -11,8 +11,16 @@ from sphinx.application import Sphinx
 
 from sphinxcontrib.{{ cookiecutter.package_name}}.version import __version__
 
+from sphinx.locale import get_translation
+
+package_dir = path.abspath(path.dirname(__file__))
+
+_ = get_translation(__name__)
+__ = get_translation(__name__, 'console')
+
 
 def setup(app: Sphinx) -> Dict[str, Any]:
+    app.add_message_catalog(__name__, path.join(package_dir, 'locales'))
     return {
         'version': __version__,
         'parallel_read_safe': True,
